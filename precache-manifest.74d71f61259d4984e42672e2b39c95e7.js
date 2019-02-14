c3d84b1f1107c61d080897cfdfd26a0247132533
m.ermoalev <m.ermolaev@itransition.com>
2019-02-14 21:34:56 +0300

    Updates

---
+++ precache-manifest.74d71f61259d4984e42672e2b39c95e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "497535408e7ee34fda06",
+    "url": "/static/js/main.49753540.chunk.js"
+  },
+  {
+    "revision": "fffdf40c1cd4f82943a4",
+    "url": "/static/js/1.fffdf40c.chunk.js"
+  },
+  {
+    "revision": "497535408e7ee34fda06",
+    "url": "/static/css/main.1b8e1c6b.chunk.css"
+  },
+  {
+    "revision": "fffdf40c1cd4f82943a4",
+    "url": "/static/css/1.a3174722.chunk.css"
+  },
+  {
+    "revision": "1d39892da7ae6217f8300070e63446ae",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cars-ground/precache-manifest.dad3ddb950657d52ed36e44abed52f1f.js"
+  "/precache-manifest.74d71f61259d4984e42672e2b39c95e7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cars-ground/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
